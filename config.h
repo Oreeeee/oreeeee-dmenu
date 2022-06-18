216a1475bc4cf69189d78da3bf98d43aa7868e22
Oreeeee <lifehackeeer2@gmail.com>
2022-06-18 14:03:20 +0200

    comment

--- config.h
+++ config.h
@@ -7,6 +7,8 @@ static const char *fonts[] = {
 	"JetBrainsMono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* Dracula theme */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f8f8f2", "#282a36" },
@@ -15,6 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNormHighlight] = { "#F1FA8C", "#282a36" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
 static unsigned int columns    = 1;
